3715489b49be120cb6dc72aa8a72ae5cc237d8f2
Brandon Bayer <b@bayer.ws>
2016-03-16 16:23:23 -0500

    feat: official fzf support & config

--- zsh-custom/config.zsh
+++ zsh-custom/config.zsh
@@ -1,11 +1,40 @@
+export EDITOR='nvim'
 
-# Preferred editor for local and remote sessions
-if [[ -n $SSH_CONNECTION ]]; then
-  export EDITOR='nvim'
-else
-  export EDITOR='nvim'
-fi
 
-# ssh
-# export SSH_KEY_PATH="~/.ssh/dsa_id"
+# ------------------------------------
+#            FZF Config
+# ------------------------------------
+export FZF_DEFAULT_COMMAND='ag -g ""'
+export FZF_CTRL_T_COMMAND='ag -g ""'
 
+# Open selected file with default editor
+nvf() {
+  local out file key
+  out=$(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e)
+  key=$(head -1 <<< "$out")
+  file=$(head -2 <<< "$out" | tail -1)
+  if [ -n "$file" ]; then
+     ${EDITOR:-nvim} "$file"
+  fi
+}
+
+# cd to selected directory
+cdf() {
+  local dir
+  dir=$(find ${1:-*} -path '*/\.*' -prune \
+                  -o -type d -print 2> /dev/null | fzf-tmux +m) &&
+  cd "$dir"
+}
+
+# git log browser
+glf() {
+  git log --graph --pretty=format:'%C(yellow)%s%Creset %an %C(blue)%cr%Creset %h%C(red)%d%Creset ' --numstat |
+  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+      --bind "ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+FZF-EOF"
+}
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
