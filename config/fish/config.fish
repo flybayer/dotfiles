c9744070c19f0e676e9454619d21c81e615aeb4f
Brandon Bayer <b@bayer.ws>
2019-06-06 10:44:00 -0400

    updates

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -29,9 +29,19 @@ alias l="ll -a"
 function nv
   bash -c "SHELL=/bin/bash nvim $argv"
 end
+alias vim="nvim"
+
+alias nci="npm -s ci"
+alias ni="npm -s install"
+alias ns="npm start"
 alias nr="npm run"
+
 alias y="yarn"
 alias ys="yarn start"
+
+alias r="bin/rails"
+alias rt="env HIDE_EXAMPLES=true bin/rails test"
+alias be="bundle exec"
 
 alias t="tree -CI 'node_modules|dist'"
 alias tree="tree -C"
@@ -46,6 +56,9 @@ alias wcr="watchexec --restart 'cargo run'"
 alias wct="watchexec --restart 'cargo test --all'"
 
 alias cdvim='cd ~/.vim'
+
+alias pss='ps aux | rg'
+
 
 #Copy latest git commit
 alias cgc='git log -1 --pretty=format:%H | xsel -b'
@@ -103,6 +116,19 @@ function fork
   git fetch --all
 end
 
+function gpu
+   set branch_name (git branch --color=never | grep \* | cut -d ' ' -f2)
+   gp -u origin $branch_name
+end
+
+function gpd
+  gp && gp origin :$argv[1] && gb -d $argv[1]
+end
+
+function gmpd
+  gm $argv[1] && gp && gp origin :$argv[1] && gb -d $argv[1]
+end
+
 
 set -x EDITOR nvim
 set -x FZF_LEGACY_KEYBINDINGS 1
@@ -110,16 +136,19 @@ set -x FZF_TMUX 1
 set -gx FZF_DEFAULT_COMMAND 'ag --ignore flow-typed -g ""'
 set -gx FZF_FIND_FILE_COMMAND "$FZF_DEFAULT_COMMAND"
 
+set -gx N_PREFIX $HOME/n
+
 set -x PGDATA /usr/local/var/postgres/
 set -e PREFIX # Fix React-native metro bundler
 set -x ANDROID_HOME $HOME/Library/Android/sdk
+set -x DISABLE_SPRING 1
 
-set -x fish_user_paths $ANDROID_HOME/emulator $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools $HOME/.fastlane/bin $fish_user_paths
+# qt@5.5 in path because it's needed by capybara-webkit which is used by bullettrain
+set -x fish_user_paths $ANDROID_HOME/emulator $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools $HOME/.fastlane/bin $fish_user_paths $N_PREFIX/bin $HOME/.rbenv/bin /usr/local/opt/qt@5.5/bin
+
+status --is-interactive; and source (rbenv init -|psub)
 
 function __direnv_export_eval --on-event fish_prompt;
         eval (direnv hook fish);
 end
 
-# tabtab source for electron-forge package
-# uninstall by removing these lines or running `tabtab uninstall electron-forge`
-[ -f /Users/b/.config/yarn/global/node_modules/tabtab/.completions/electron-forge.fish ]; and . /Users/b/.config/yarn/global/node_modules/tabtab/.completions/electron-forge.fish
